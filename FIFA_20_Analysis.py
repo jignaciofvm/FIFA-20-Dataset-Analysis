e498c4fdfea440c1a48d6f653043139aafbca6b3
José Ignacio Fernández Villafáñez <77568333+jignaciofvm@users.noreply.github.com>
2021-07-13 16:46:26 +0200

    Update FIFA_20_Analysis.py

--- FIFA_20_Analysis.py
+++ FIFA_20_Analysis.py
@@ -777,8 +777,8 @@ with model:
 
     numeric_variables = ['Age', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY', 'PAC']
     categorical_variables = ['position_fe','Club']
-    model = Path(__file__).parents[0] / 'files/model_XGB.sav'
-    loaded_model = pickle.load(open(model, 'rb'))
+    # model = Path(__file__).parents[0] / 'files/model_XGB.sav'
+    loaded_model = pickle.load(open("model_XGB.sav", 'rb'))
     data = [age, position_fe, IR, SHO, PAS, DRI, DEF, PHY, PAC, Club]
     df_model = pd.DataFrame([data], columns = ['Age','position_fe','IR','SHO', 'PAS', 'DRI', 'DEF', 'PHY', 'PAC', 'Club'])
     # st.write(np.exp(loaded_model.predict(df_model)))
