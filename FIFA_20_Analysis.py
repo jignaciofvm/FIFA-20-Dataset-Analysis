e1bd1b603503d2dbc548d51c242cb91755735c3c
José Ignacio Fernández Villafáñez <77568333+jignaciofvm@users.noreply.github.com>
2021-07-13 11:39:11 +0200

    Update FIFA_20_Analysis.py

--- FIFA_20_Analysis.py
+++ FIFA_20_Analysis.py
@@ -32,8 +32,8 @@ with dataset:
     col_names_df = ["Name","Image","Country","Position","Age","Overall","Potential","Club","ID","Height","Weight","foot","BOV","BP","Growth","Joined","Loan Date End",
     "Value","Wage","Release Clause","Attacking","Crossing","Finishing","Heading Accuracy","Short Passing","Volleys","Skill","Dribbling",
     "Curve","FK Accuracy","Long Passing","Ball Control","Movement","Acceleration","Sprint Speed","Agility","Reactions","Balance","Power","Shot Power","Jumping","Stamina","Strength","Long"]
-    df = pd.read_csv('FIFA-20-Dataset-Analysis/files/fifa20_data.csv', names = col_names_df)
-    df_similarity = pd.read_csv('FIFA-20-Dataset-Analysis/files/df_similarity.csv')
+    df = pd.read_csv('https://github.com/jignaciofvm/FIFA-20-Dataset-Analysis/blob/main/files/fifa20_data.csv', names = col_names_df)
+    df_similarity = pd.read_csv('https://github.com/jignaciofvm/FIFA-20-Dataset-Analysis/blob/main/files/df_similarity.csv')
     st.write(df.head())
     st.text("""
         ES: Comentar que el dataset anterior ha sido tratado. Si quiere seguir el proyecto, acuda
